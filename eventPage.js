1c4cf33d182c1278500b360dfbacbcdf5b0dcc8e
Yuyue Zhu <yuyuez@uw.edu>
2018-03-10 00:36:16 -0800

    added notification and cancel

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,18 @@
-
 chrome.alarms.onAlarm.addListener( function(alarm){
-    chrome.alarms.clearAll();
-    chrome.windows.getCurrent(function(w) {
-        chrome.windows.remove(w.id);
-    })
+    if(alarm.name == "oneMinute"){
+        chrome.notifications.create('reminder',
+        {
+            type: 'basic',
+            title: 'Enforcement Incoming',
+            message: 'You have 1 minute left',
+            iconUrl: 'icon.png'
+        }, function(notificationId){});
+    }
+    else{
+        chrome.alarms.clearAll();
+        chrome.windows.getCurrent(function(w) {
+            chrome.windows.remove(w.id);
+        })
+    }
 })
+
--- popup.js
+++ popup.js
@@ -5,20 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
     restore();
   });
   document.getElementById('submit').addEventListener('click', () => {
-    if(input.value > 1){
+    if(input.value > 0){
       set_auto_close(input.value);
+      if(input.value >= 1){
+        chrome.alarms.create("oneMinute",{when:Date.now()+ (input.value - 1)*60000})
+      }
+      chrome.notifications.create('reminder',
+      {
+        type: 'basic',
+        title: 'Enforcement Incoming',
+        message: 'Less than 1 minute left',
+        iconUrl: 'icon.png'
+      }, function(notificationId){});
+    } else {
+      alert("Invalid Input");
     }
+  });
+  document.getElementById('cancel').addEventListener('click', () => {
+    cancel();
   });
 });
 
 function set_auto_close(time) {
-  let minutes = time/60;
-  chrome.alarms.create({delayInMinutes:minutes, periodInMinutes:minutes})
+  chrome.alarms.create({when:Date.now()+time*60000});
 }
+
 function restore(){
   chrome.sessions.getRecentlyClosed(function(s){
     chrome.sessions.restore(s[0].sessionId);
   })
 }
 
-
+function cancel(){
+  chrome.alarms.clearAll();
+}
