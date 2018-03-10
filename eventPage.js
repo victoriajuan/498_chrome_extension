578372f14e7deefccd367aebb4cdcb736e1e0377
Vicky Juan <vicky37@uw.edu>
2018-03-09 23:45:28 -0800

    adding notification

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,19 @@
-
 chrome.alarms.onAlarm.addListener( function(alarm){
-    chrome.alarms.clearAll();
-    chrome.windows.getCurrent(function(w) {
-        chrome.windows.remove(w.id);
-    })
+    if (alarm.name == "oneMinuteAlarm") {
+        // chrome.alarms.clear("oneMinuteAlarm");
+        var options = {
+            type: 'basic',
+            title: 'keep burning',
+            message: 'You have 1 minutes left',
+            iconUrl:'icon.png'
+          };
+          chrome.notifications.create('reminder',options, function(notificationId){
+            console.log("!!!!!!!");
+          });
+    } else if(alarm.name == "autoCloseAlarm") {
+        chrome.alarms.clearAll();
+        chrome.windows.getCurrent(function(w) {
+            chrome.windows.remove(w.id);
+        });
+    }
 })
--- popup.js
+++ popup.js
@@ -5,15 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
     restore();
   });
   document.getElementById('submit').addEventListener('click', () => {
-    if(input.value > 1){
+    if(input.value >= 1){
+      //when the time set by user is equal to 1 minute
+      if(input.value == 1) {
+        var options = {
+          type: 'basic',
+          title: 'Time saver',
+          message: 'You have 1 minutes left',
+          iconUrl:'icon.png'
+        };
+        chrome.notifications.create('reminder',options, function(notificationId){
+          console.log("!!!!!!!");
+        });
+      }else { 
+        chrome.alarms.create("oneMinuteAlarm", {delayInMinutes:input.value - 1})
+      }
       set_auto_close(input.value);
     }
   });
 });
 
 function set_auto_close(time) {
-  let minutes = time/60;
-  chrome.alarms.create({delayInMinutes:minutes, periodInMinutes:minutes})
+  let minutes = time;
+  chrome.alarms.create("autoCloseAlarm", {delayInMinutes:minutes, periodInMinutes:minutes})
 }
 function restore(){
   chrome.sessions.getRecentlyClosed(function(s){
